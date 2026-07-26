db707e97ad42f4a01f2f469b2054f3251edcac1c
kailashramkalbi70-oss <kailashramkalbi70@gmail.com>
2026-07-26 23:54:46 +0530

    Update payment.js

--- payment.js
+++ payment.js
@@ -1,1 +1,76 @@
+function completePayment(){
 
+const transactionId =
+document.getElementById("transactionId").value.trim();
+
+if(!transactionId){
+
+alert("Please enter Transaction ID.");
+
+return;
+
+}
+
+const pending =
+JSON.parse(
+localStorage.getItem("pendingOrder")
+);
+
+if(!pending){
+
+alert("No pending order found.");
+
+location.href="plans.html";
+
+return;
+
+}
+
+const orders =
+JSON.parse(
+localStorage.getItem("orders") || "[]"
+);
+
+const orderId =
+"IG" +
+Date.now().toString().slice(-8);
+
+const newOrder = {
+
+id:orderId,
+
+plan:pending.plan,
+
+price:pending.price,
+
+instagramUrl:pending.instagramUrl,
+
+playerName:pending.playerName,
+
+transactionId,
+
+status:"Pending",
+
+createdAt:new Date().toISOString()
+
+};
+
+orders.push(newOrder);
+
+localStorage.setItem(
+"orders",
+JSON.stringify(orders)
+);
+
+localStorage.setItem(
+"lastOrderId",
+orderId
+);
+
+localStorage.removeItem(
+"pendingOrder"
+);
+
+location.href="order-success.html";
+
+}
