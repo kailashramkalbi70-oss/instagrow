cbb677a1b79cde44e0cbb6910a25cb7abe56c9ad
kailashramkalbi70-oss <kailashramkalbi70@gmail.com>
2026-07-26 23:53:19 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,33 @@
+function toggleMenu(){
 
+const menu =
+document.getElementById("mobileMenu");
+
+if(!menu) return;
+
+if(menu.style.display === "block"){
+menu.style.display = "none";
+}else{
+menu.style.display = "block";
+}
+
+}
+
+document.addEventListener("DOMContentLoaded",()=>{
+
+const user =
+JSON.parse(localStorage.getItem("currentUser"));
+
+const loginLinks =
+document.querySelectorAll('a[href="login.html"]');
+
+if(user){
+
+loginLinks.forEach(link=>{
+link.textContent="Profile";
+link.href="profile.html";
+});
+
+}
+
+});
